b3dabac822939f76fef33db3c9250af9e92f64a7
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-02-03 02:26:08 -0500

    current backup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'MobileAR' });
 });
+
+/**GET the app window */
+router.get('/app', function(req, res, next) {
+    res.render('app', {
+        title: 'APPWindow'
+    });
+})
 
 module.exports = router;
